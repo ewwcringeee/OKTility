305c091507c47661f46cbe6c89b10ea723b92907
ewwcringeee <hotdick@internet.ru>
2024-08-27 15:41:42 +0300

    Update load.lua

--- load.lua
+++ load.lua
@@ -1,1 +1,8 @@
-repo = https://raw.githubusercontent.com/ewwcringeee/OKTility/main/""
+Repo = "https://raw.githubusercontent.com/ewwcringeee/OKTility/main/"
+Get = game:HttpGet
+
+function GetM(module)
+  get(Repo..module)
+end
+
+GetM("main.lua")
